a838b88036566a46452c23fecf7ceee76621ef4f
taytaoto <tayto@taytaoto.lan>
2025-01-18 00:17:47 +0700

    update color of selected area

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,8 +2,10 @@ local wezterm = require 'wezterm'
 local config = {}
 
 config.color_scheme = 'Everforest Light Medium (Gogh)'
-
-
+config.colors = {
+	selection_bg = '#d79921', -- Orange background for the selection
+	selection_fg = '#ffffff'
+}
 config.font = wezterm.font('JetBrainsMono Nerd Font Mono')
 config.font_size = 11
 --config.line_height = 1.1
